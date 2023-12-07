785b3e405f1da905e49dd4109b8756b1e85c6b7e
Jason Liu <it@liujinshui.com>
2023-12-07 22:54:41 +0800

    feat 事物定义

---
+++ 1-1-2.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(define size 2)
+
+size
+
+(* 5 size)
+
+(define pi 3.14159)
+; 半径
+(define radius 10)
+
+(* pi (* radius radius))
+
+; 周长
+(define circumference (* 2 pi radius))
+
+circumference
